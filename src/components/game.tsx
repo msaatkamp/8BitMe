f179552a1ed8e10d215e041d847a2dcce7309f88
Matheus Saatkamp <matheus.saatakmp@intelie.com.br>
2020-09-12 16:52:43 -0300

    Adding Tileset to Canvas

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useContext } from 'react'
+import React, { useRef, useEffect, useContext, useState } from 'react'
 import styled from 'styled-components'
 import { GameState } from '../Main'
 
@@ -17,21 +17,68 @@ type gameProps = {
     save?: object
 }
 
+
+let gameMap = [
+    [12, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [12, 1, 2, 10, 11, 12, 13, 14, 15, 16],
+    [12, 1, 2, 17, 18, 19, 20, 21, 22, 23],
+    [12, 1, 2, 0, 0, 0, 0, 0, 0, 5],
+    [12, 1, 2, 1, 0, 0, 0, 0, 0, 5]
+]
+
+const tileSize: number = 32;
+
+const getTile = () => {
+
+
+}
+
+
 const Game: React.FC<gameProps> = ({ save }) => {
-    let context = null;
-    let gameMap = [
-        0, 0, 1, 0, 0,
-        0, 1, 1, 0, 0,
-        0, 1, 0, 0, 0,
-        0, 1, 0, 0, 0,
-        0, 1, 1, 1, 2
-    ]
-
-    const canvasRef = useRef(null)
-
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const [loading, setLoading] = useState<boolean>(true)
 
     useEffect(() => {
         console.log('on mount hook')
+        if (canvasRef.current) {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext('2d')
+            const tileSet: HTMLImageElement = new Image()
+            const tileSetRowSize = 8;
+            const tileSetColSize = 8;
+            tileSet.src = "../assets/imgs/room.gif"
+            console.log(`image urL `, "localhost:3000/assets/imgs/room.gif")
+            tileSet.onload = () => {
+
+                console.log(`carrego essa porra`)
+                gameMap.map((row, y) => {
+                    row.forEach((id, x) => {
+                        console.log(`x, y `, { x, y, tileSize, id, row })
+                        if (context) {
+
+                            const xPos = x * tileSize;
+                            const yPos = y * tileSize;
+                            const tile = id
+                            const tileRow = (tile / tileSetRowSize) | 0
+                            const tileCol = (tile % tileSetColSize) | 0
+                            console.log(`Cant read property: `, { tileRow, tileCol })
+                            context.drawImage(tileSet, (tileCol * tileSize), (tileRow * tileSize), tileSize, tileSize, (xPos), (yPos), tileSize, tileSize);
+                            /*
+                            context.save()
+                            context.beginPath()
+                            context.strokeRect(xPos, yPos, 32, 32)
+                            context.fillStyle = context && !id ? "orange" : id === 1 ? "yellow" : id === 5 ? "pink" : "red"
+                            context.fillRect(xPos, yPos, tileSize, tileSize)
+                            context.stroke()
+                            context.restore()
+                            context.closePath()*/
+                        }
+
+                    })
+                })
+                setLoading(false)
+            }
+        }
     }, [])
 
     const gameContext = useContext(GameState)
@@ -41,12 +88,15 @@ const Game: React.FC<gameProps> = ({ save }) => {
         <Container>
             <canvas
                 ref={canvasRef}
-                hidden={true}
+                hidden={loading}
                 onClick={(x) => {
-                    const me = x
+                    const me = { ...x }
                     const canvas = canvasRef.current
-                    console.log(`CLicked at `, me, ` with keys ${Object.keys(me)} ref: ${canvas}`)
-                }}></canvas >
+                    console.log(`CLicked at `, { me }, ` with keys ${Object.keys(me)} ref: ${canvas}`)
+                }}
+                width={640}
+                height={320}
+            ></canvas >
         </Container>
     )
 }
@@ -56,11 +106,14 @@ const Container: React.FC = styled.div`
     height: 50vh;
     display: flex;
     align-self: center;
+    justify-content: center;
     > canvas {
-        width: 100%;
-        height: auto;
-        border: solid 1px white;
-        opacity: 50%;
+        display: flex;
+        width: 50vw;
+        height: 50vh;
+        /* border: solid 1px white; */
+        opacity: 100%;
+        padding: 12px;
     }
 `
 
